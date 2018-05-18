a913ea550215a1c3336a30e283933cbd7029412f
wulei <wulei_clec@chinalife.com.cn>
2018-05-18 11:31:13 +0800

    init

---
+++ src/main/java/Servers/EchoServer.java
@@ -0,0 +1,51 @@
+package Servers;
+
+import handlers.EchoServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+    public static void main(String[] args) throws Exception{
+        if(args.length !=1){
+            System.err.println("Usage:"+EchoServer.class.getSimpleName()+
+                    " ");
+        }
+        int port = Integer.parseInt(args[0]);// 设置端口值
+        new EchoServer(port).start();
+    }
+    public void start() throws Exception{
+        final EchoServerHandler echoServerHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();// 创建loopgroup
+        try {
+            ServerBootstrap b = new ServerBootstrap();//创建serverbootstrap
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)//指定所使用的NIO传输Channel
+                    .localAddress(new InetSocketAddress(port))//使用指定的端口设置套接字地址
+                    .childHandler(new ChannelInitializer<SocketChannel>() {//添加一个EchoServer-Handler到
+                        //子Channel 的ChannelPipeline
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(echoServerHandler);
+                        }
+                    });
+            ChannelFuture f = b.bind().sync(); //异步地绑定服务器；调用sync()方法阻塞等待直到绑定完成
+            f.channel().closeFuture().sync(); //获取Channel的closeFurure，并且阻塞当前线程直到它完成
+        }finally {
+            group.shutdownGracefully().sync();//关闭EventLoopGroup,释放所有资源
+        }
+
+    }
+}
